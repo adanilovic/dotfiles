a1fc591a8cddca18fff64dffa5279c22edf55f14
Andrew Danilovic <a.danilovic@ucla.edu>
2018-04-22 21:03:22 -0700

    Remove download_plugins.sh, since this functionality was moved to vimrc

--- vim/.vim/download_plugins.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-curl -fLo ./autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-#Download taglist version 4.6
-curl -fLo taglist_46.zip --create-dirs https://vim.sourceforge.io/scripts/download_script.php?src_id=19574
-unzip taglist_46.zip
